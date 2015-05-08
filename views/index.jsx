d1e535cb67d17da507621e372f6bbabbf0ba16eb
fand <fand@gmork.in>
2015-05-09 02:57:09 +0900

    ex3

--- views/index.jsx
+++ views/index.jsx
@@ -13,17 +13,32 @@ var TodoBox = React.createClass({
 });
 
 var TodoList = React.createClass({
-  render: function () {
+  render: function() {
     return (
-      <div className="todoList">
-        I am a TodoList.
+      <div className = "todoList">
+        <table style={{border: "2px solid black"}}>
+          <tbody>
+            <Todo title="Shopping">Milk</Todo>
+            <Todo title="Hair cut">13:00</Todo>
+          </tbody>
+        </table>
       </div>
     );
   }
 });
 
+var Todo = React.createClass({
+  render: function () {
+    return (
+      <tr>
+        <td style={{border: '1px solid black'}}>{this.props.title}</td>
+        <td style={{border: '1px solid black'}}>{this.props.children}</td>
+      </tr>
+    );
+  }
+});
+
 var TodoForm = React.createClass({
-  // ここに記述
   render: function () {
     return (
       <div className="todoForm">
