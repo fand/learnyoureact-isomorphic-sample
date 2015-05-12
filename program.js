d5b0bbf651be814b30e397474e5e3a86837f750c
fand <fand@gmork.in>
2015-05-13 01:11:13 +0900

    ex7

--- program.js
+++ program.js
@@ -6,8 +6,16 @@ app.set('view engine', 'jsx');
 app.set('views', __dirname + '/views');
 app.engine('jsx', require('express-react-views').createEngine());
 
+var data = [{
+  title: 'Shopping',
+  detail: process.argv[3]
+}, {
+  title: 'Hair cut',
+  detail: process.argv[4]
+}];
+
 app.use('/', function(req, res) {
-  res.render('index', '');
+  res.render('index', {data: data});
 });
 
 app.listen(app.get('port'), function() {});
--- views/index.jsx
+++ views/index.jsx
@@ -5,7 +5,7 @@ var TodoBox = React.createClass({
     return (
       <div className="todoBox">
         <h1>Todos</h1>
-        <TodoList />
+        <TodoList data = {this.props.data} />
         <TodoForm />
       </div>
     );
@@ -14,12 +14,14 @@ var TodoBox = React.createClass({
 
 var TodoList = React.createClass({
   render: function() {
+    var todo = this.props.data.map(function(obj) {
+      return <Todo title={obj.title} key={obj.title}>{obj.detail}</Todo>;
+    });
     return (
       <div className = "todoList">
         <table style={{border: "2px solid black"}}>
           <tbody>
-            <Todo title="Shopping">Milk</Todo>
-            <Todo title="Hair cut">13:00</Todo>
+            {todo}
           </tbody>
         </table>
       </div>
