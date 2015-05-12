d558b3fa4c03214e43106853f992d71b3304a82e
fand <fand@gmork.in>
2015-05-13 00:41:42 +0900

    ex4

--- views/index.jsx
+++ views/index.jsx
@@ -28,11 +28,14 @@ var TodoList = React.createClass({
 });
 
 var Todo = React.createClass({
-  render: function () {
+  propTypes: {
+    title: React.PropTypes.string.isRequired
+  },
+  render: function() {
     return (
       <tr>
-        <td style={{border: '1px solid black'}}>{this.props.title}</td>
-        <td style={{border: '1px solid black'}}>{this.props.children}</td>
+        <td style={{border: "1px solid black"}}>{this.props.title}</td>
+        <td style={{border: "1px solid black"}}>{this.props.children}</td>
       </tr>
     );
   }
